624940c6e8ad8ed7044e105a624673e92cea54b0
Turdibek <turdibekdev05@gmail.com>
2022-06-13 15:11:29 +0500

    Add app.py

---
+++ bot/app.py
@@ -0,0 +1,13 @@
+from aiogram import executor
+
+from loader import dp
+from utils import on_startup_notify
+from utils import set_default_commands
+
+
+async def onStartup(dispatcher):
+    await set_default_commands(dispatcher)
+    await on_startup_notify(dispatcher)
+
+if __name__ == "__main__":
+    executor.start_polling(dp, on_startup=onStartup)
